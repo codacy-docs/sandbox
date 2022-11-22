8c32d4bf2959d270d3c0301f8039785400fb173c
nicklem <nicola.klemenc@codacy.com>
2022-11-22 11:11:01 +0100

    Decrease coverage

--- samplePHPProject/src/UserTest.php
+++ samplePHPProject/src/UserTest.php
@@ -8,17 +8,17 @@ use App\User;
 
 class UserTest extends TestCase
 {
-    public function testTrue()
-    {
-        $user = new User();
-        $this->assertTrue($user->returnTrue());
-        return 1;
-    }
-
-    public function testFalse()
-    {
-        $user = new User();
-        $this->assertFalse($user->returnFalse());
-        return 1;
-    }
+//    public function testTrue()
+//    {
+//        $user = new User();
+//        $this->assertTrue($user->returnTrue());
+//        return 1;
+//    }
+//
+//    public function testFalse()
+//    {
+//        $user = new User();
+//        $this->assertFalse($user->returnFalse());
+//        return 1;
+//    }
 }
