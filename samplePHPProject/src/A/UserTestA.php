e2268374030ba600f6d7e0d26f547c9ef8d70b58
nicklem <nicola.klemenc@codacy.com>
2022-11-23 15:46:40 +0100

    Set up multiple coverage reports

---
+++ samplePHPProject/src/A/UserTestA.php
@@ -0,0 +1,24 @@
+<?php
+
+// phpcs:ignore
+require_once '../../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use App\A\User as UserA;
+
+class UserTestA extends TestCase
+{
+//    public function testTrue()
+//    {
+//        $user = new UserA();
+//        $this->assertTrue($user->returnTrue());
+//        return 1;
+//    }
+
+//    public function testFalse()
+//    {
+//        $user = new UserA();
+//        $this->assertFalse($user->returnFalse());
+//        return 1;
+//    }
+}
---
+++ samplePHPProject/src/B/UserTestB.php
@@ -0,0 +1,24 @@
+<?php
+
+// phpcs:ignore
+require_once '../../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use App\B\User as UserB;
+
+class UserTestB extends TestCase
+{
+//    public function testTrue()
+//    {
+//        $user = new UserB();
+//        $this->assertTrue($user->returnTrue());
+//        return 1;
+//    }
+
+//    public function testFalse()
+//    {
+//        $user = new UserB();
+//        $this->assertFalse($user->returnFalse());
+//        return 1;
+//    }
+}
